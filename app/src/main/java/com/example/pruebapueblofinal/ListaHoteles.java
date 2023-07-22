0434ce5d224ed411505e7c4e3b77593e9091c8a8
Juan <juanpabloo2141@gmail.com>
2023-07-21 20:43:05 -0500

    ok

--- app/src/main/java/com/example/pruebapueblofinal/ListaHoteles.java
+++ app/src/main/java/com/example/pruebapueblofinal/ListaHoteles.java
@@ -1,14 +1,37 @@
 package com.example.pruebapueblofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class ListaHoteles extends AppCompatActivity {
+import com.example.pruebapueblofinal.adaptadores.HotelAdaptador;
+import com.example.pruebapueblofinal.moldes.MoldeHotel;
 
+import java.util.ArrayList;
+
+public class ListaHoteles extends AppCompatActivity {
+    ArrayList<MoldeHotel> listaHoteles=new ArrayList<>();
+    RecyclerView recyclerHoteles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_hoteles);
+        recyclerHoteles=findViewById(R.id.listadinamicahoteles);
+        recyclerHoteles.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+
+        crearLista();
+        HotelAdaptador adaptador=new HotelAdaptador(listaHoteles);
+        recyclerHoteles.setAdapter(adaptador);
+
+    }
+
+    public void crearLista(){
+        listaHoteles.add(new MoldeHotel("Hotel1","250000",R.drawable.hotel1));
+        listaHoteles.add(new MoldeHotel("Hotel2","350000",R.drawable.hotel2));
+        listaHoteles.add(new MoldeHotel("Hotel3","450000",R.drawable.hotel3));
+        listaHoteles.add(new MoldeHotel("Hotel4","550000",R.drawable.hotel4));
+        listaHoteles.add(new MoldeHotel("Hotel5","650000",R.drawable.hotel1));
     }
 }
